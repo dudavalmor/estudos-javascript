81aa0639650ae31448156a1d91d03c78053a352a
dudavalmor <eduarda.bserrano@gmail.com>
2024-11-24 04:05:28 -0300

    Commit de Estruturas de controle

---
+++ estruturas de controle/do while.js
@@ -0,0 +1,6 @@
+let i = 1
+
+do{
+    console.log(`Passo ${i}`)
+    i++
+} while(i <= 10)
---
+++ estruturas de controle/ex008.js
@@ -0,0 +1,6 @@
+let vel = 12
+console.log(`A velocidade do seu carro é de ${vel}km/h.`)
+if(vel > 60){ //condição simples
+    console.log(`Você passou a velocidade permitida. MULTADO!`)
+}
+console.log(`Dirija sempre usando cinto de segurança`)
